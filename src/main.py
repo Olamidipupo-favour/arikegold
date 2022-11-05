9ae656b9f5f072990092558d7da0c1e17e9da834
Agboola Olamidipupo Favour <olamidipupofavour@codecube.com.ng>
2022-11-05 10:30:15 +0100

    finished onboboarding design

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import MDScreen
@@ -6,8 +7,11 @@ sm=ScreenManager()
 class Onboarding(MDScreen):
 	pass
 class Main(MDApp):
+	data=os.getcwd()+'/data/'
+	#change on production
 	def build(self):
 		global sm
 		sm.add_widget(Onboarding(name="page0"))
+		sm.current="page0"
 if __name__=='__main__':
 	Main().run()
